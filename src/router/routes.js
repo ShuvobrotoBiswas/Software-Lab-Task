0fa375b71f8ef4ed63cb142dcd8f6367317eeb00
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-07-10 00:44:42 +0600

    update

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,14 @@ const routes = [
       { path: '/', component: () => import('src/pages/loginPage.vue') },
       { path: '/signup', component: () => import('src/pages/signupPage.vue') },
       { path: '/index', component: () => import('pages/IndexPage.vue') },
+      { path: '/forgotPassword', component: () => import('pages/forgotPassword.vue') },
+      { path: '/otpVerify', component: () => import('pages/otpVerify.vue') },
+      { path: '/resetPassword', component: () => import('pages/resetPassword.vue') },
+      { path: '/signupPage1', component: () => import('pages/signupPage1.vue') },
+      { path: '/signupPage2', component: () => import('pages/signupPage2.vue') },
+      { path: '/signupPage3', component: () => import('pages/signupPage3.vue') },
+      { path: '/signupPage4', component: () => import('pages/signupPage4.vue') },
+      { path: '/signupPage5', component: () => import('pages/signupPage5.vue') },
     ]
   },
 
